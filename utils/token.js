d6a2e5d6c1296e46c74ab55bf1de7f95ec989d7d
dafolk <kaungsoesan01@gmail.com>
2023-07-25 17:17:08 +0630

    token utilities

---
+++ utils/token.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (username, secretKey) => {
+  return jwt.sign({ username: username }, secretKey, { expiresIn: "24h" });
+};
+
+const isTokenExpired = (token, secretKey) => {
+  let bool = false;
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      if (err.name == "TokenExpiredError") {
+        bool = true;
+      }
+    }
+  });
+  return bool;
+};
+
+module.exports = { generateToken };
