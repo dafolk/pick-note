1a7f1e2503e21e04ed1a1b6de7d8052138774755
dafolk <kaungsoesan01@gmail.com>
2023-07-26 11:27:28 +0630

    use cors

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const glob = require("glob");
 const authorization = require("./middleware/auth-middleware");
+const cors = require("cors");
 
 const PORT = 4003;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(authorization);
 
